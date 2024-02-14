5f0260d0f876e678afaf9784d5fbe02657a03e44
Aditya-Devarkonda <123838871+Aditya-Devarkonda@users.noreply.github.com>
2024-02-14 17:05:09 +0530

    all done according to the scrimba

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,55 +1,77 @@
 import React from "react";
 
 export function Meme() {
-	// https://api.imgflip.com/get_memes
 	const [inputData, setInputData] = React.useState({
 		topText: "",
 		bottomText: "",
-		memeUrl: ""
+		memeUrl: "",
 	});
 
 	const [memeArray, setMemeArray] = React.useState([]);
 
-	React.useEffect(()=>{
+	React.useEffect(() => {
 		fetch("https://api.imgflip.com/get_memes")
-		.then(res => res.json())
-		.then(data => setMemeArray(data.data.memes))
-	},[])
+			.then((res) => res.json())
+			.then((data) => setMemeArray(() => data.data.memes));
+	}, []);
 
-	function getRandomMemeImage(){
-		const number = Math.floor(Math.random() * memeArray.length)
-		const imgUrl = memeArray[number].url
-		console.log(imgUrl)
-		setInputData(prevdata => ({
+	function getRandomMemeImage() {
+		const number = Math.floor(Math.random() * memeArray.length);
+		const imgUrl = memeArray[number].url;
+		setInputData((prevdata) => ({
 			...prevdata,
-			memeUrl: imgUrl
-		}))
+			memeUrl: imgUrl,
+		}));
+	}
+
+	function handleInputChnage(event) {
+		const { name, value } = event.target;
+		setInputData((prevdata) => ({
+			...prevdata,
+			[name]: value,
+		}));
 	}
 
 	return (
 		<main className="Meme-div">
 			<div className="Meme-form">
 				<div>
-					<lebel className="Meme-left-lebel" for="lebel1">
-						Top text
-					</lebel>
-					<input type="text" placeholder="Shut up" className="Meme-left-input" id="lebel1" value={inputData.topText}/>
-				</div>
-				<div>
-					<lebel className="Meme-right-lebel" for="lebel2">
-						Bottom text
-					</lebel>
+					<label className="Meme-left-lebel" htmlFor="lebel1">
+						Add text to meme
+					</label>
 					<input
 						type="text"
-						placeholder="And take my money"
+						placeholder=""
+						className="Meme-left-input"
+						id="lebel1"
+						name="topText"
+						onChange={handleInputChnage}
+						value={inputData.topText}
+					/>
+				</div>
+				<div>
+					<label className="Meme-right-lebel" htmlFor="lebel2">
+						Add bottom text
+					</label>
+					<input
+						type="text"
+						placeholder=""
 						className="Meme-right-input"
 						id="lebel2"
+						name="bottomText"
+						onChange={handleInputChnage}
 						value={inputData.bottomText}
 					/>
 				</div>
-				<button className="Meme-submit-button" onClick={getRandomMemeImage}>Get a new meme image 🖼</button>
+				<button className="Meme-submit-button" onClick={getRandomMemeImage}>
+					Get a new meme image 🖼
+				</button>
 			</div>
-			<img className="memeImage" src={inputData.memeUrl}></img>
+			<div className="memeImageContainer">
+				<img className="memeImage" src={inputData.memeUrl}></img>
+				<h2 className="memeText top">{inputData.topText}</h2>
+				<h2 className="memeText bottom">{inputData.bottomText}</h2>
+			</div>
 		</main>
 	);
 }
